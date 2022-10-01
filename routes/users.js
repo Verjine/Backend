89c44486e5e137c652c8297f4d690a6beb5bbd4e
Verjine <verjine.pipoyan@gmail.com>
2022-10-01 11:12:09 +0400

    project

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import { getUsers, createUser,  deleteUser, updateUser } from '../controllers/users.js';
+
+const router = express.Router();
+
+router.get('/', getUsers);
+
+router.post('/', createUser);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
+export default router;
